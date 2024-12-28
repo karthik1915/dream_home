fcaf1c5aaf22eb259b862a3d8abcd48a9e4c60b8
Karthikeyan J <karthikeyanj1915@gmail.com>
2024-12-28 12:41:57 +0530

    deployment alteration
    configured application dependencies and linted for deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${alfaSlab.variable} antialiased `}
+        className={`${geistSans.variable} ${alfaSlab.variable} ${humantBold.variable} antialiased `}
       >
         {children}
       </body>
